772e2a5c1c37b7ba7841b4dcccbeffd7b653ab68
Ryan Grunest <ryangrunest45@gmail.com>
2019-08-08 15:43:58 -0700

    added react-konva

---
+++ shared_whiteboard_client/src/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import { Stage, Layer, Rect, Text } from 'react-konva';
+import Konva from 'konva';
+
+class ColoredRect extends React.Component {
+  state = {
+    color: 'green'
+  };
+  handleClick = () => {
+    this.setState({
+      color: Konva.Util.getRandomColor()
+    });
+  };
+  render() {
+    return (
+      <Rect
+      draggable
+        x={100}
+        y={100}
+        width={1250}
+        height={600}
+        fill={this.state.color}
+        shadowBlur={5}
+        onClick={this.handleClick}
+      />
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    // Stage is a div container
+    // Layer is actual canvas element (so you may have several canvases in the stage)
+    // And then we have canvas shapes inside the Layer
+    return (
+      <Stage width={window.innerWidth} height={window.innerHeight}>
+        <Layer>
+          <Text text="Try click on rect" />
+          <ColoredRect />
+        </Layer>
+      </Stage>
+    );
+  }
+}
+
+export default App;
