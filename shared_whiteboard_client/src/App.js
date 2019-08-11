833303d6643cabff486923c4f370f94947808289
Ryan Grunest <ryangrunest45@gmail.com>
2019-08-11 10:29:08 -0700

    import react-sketch

--- shared_whiteboard_client/src/App.js
+++ shared_whiteboard_client/src/App.js
@@ -1,45 +1,58 @@
 import React, { Component } from 'react';
+import './index.css';
 import { render } from 'react-dom';
 import { Stage, Layer, Rect, Text } from 'react-konva';
 import Konva from 'konva';
+import { SketchField, Tools } from 'react-sketch';
 
-class ColoredRect extends React.Component {
-  state = {
-    color: 'green'
-  };
-  handleClick = () => {
-    this.setState({
-      color: Konva.Util.getRandomColor()
-    });
-  };
-  render() {
-    return (
-      <Rect
-      draggable
-        x={100}
-        y={100}
-        width={1250}
-        height={600}
-        fill={this.state.color}
-        shadowBlur={5}
-        onClick={this.handleClick}
-      />
-    );
+// class ColoredRect extends React.Component {
+//   state = {
+//     color: 'green'
+//   };
+//   handleClick = () => {
+//     this.setState({
+//       color: Konva.Util.getRandomColor()
+//     });
+//   };
+//   render() {
+//     return (
+//       <Rect
+//       draggable
+//         x={100}
+//         y={100}
+//         width={1250}
+//         height={600}
+//         fill={this.state.color}
+//         shadowBlur={5}
+//         onClick={this.handleClick}
+//       />
+//     );
+//   }
+// }
+
+class SketchFieldDemo extends React.Component {
+  handleClick(e) {
+    e.preventDefault();
+    console.log(e);
   }
-}
+  render() {
+     return (
+         <SketchField width='100%' 
+                      height='2000px' 
+                      tool={Tools.Pencil} 
+                      lineColor='black'
+                      lineWidth={3}
+                      onClick={() => this.handleClick}/>
+     )
+  }
+};
 
 class App extends Component {
   render() {
-    // Stage is a div container
-    // Layer is actual canvas element (so you may have several canvases in the stage)
-    // And then we have canvas shapes inside the Layer
     return (
-      <Stage width={window.innerWidth} height={window.innerHeight}>
-        <Layer>
-          <Text text="Try click on rect" />
-          <ColoredRect />
-        </Layer>
-      </Stage>
+      <div className="App">
+        <SketchFieldDemo />
+      </div>
     );
   }
 }
