67426f01090a12e1f59642a91dbd9a59ad5e33d5
Ryan Grunest <ryangrunest45@gmail.com>
2019-08-11 11:37:52 -0700

    create loadData funcntionn

--- shared_whiteboard_client/src/App.js
+++ shared_whiteboard_client/src/App.js
@@ -1,37 +1,34 @@
 import React, { Component } from 'react';
 import './index.css';
-// import { SketchField, Tools } from 'react-sketch';
 import CanvasDraw from 'react-canvas-draw';
 
-// class SketchFieldDemo extends Component {
-//   handleClick(e) {
-//     e.preventDefault();
-//     console.log(e);
-//   }
-//   render() {
-//      return (
-//          <SketchField width='100px' 
-//                       height='100px' 
-//                       tool={Tools.Pencil} 
-//                       lineColor='black'
-//                       lineWidth={3}
-//                       onClick={() => this.handleClick}/>
-//      )
-//   }
-// };
-
-
-
-
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: ""
+    }
+  }
+
+  // to load data 
+  loadData() {
+    let dbData; // returned data from db
+    this.setState({
+      data: dbData
+    }, () => this.refs.canvas.loadSaveData(this.state.dbData, true))
+    // loadSaveData(saveData:String, immediata:Boolean) boolean flag loads immediately instead of live drawing it
+  }
+  
   handleClick() {
-    let savedData = this.refs.canvas.getSaveData();
-    alert(savedData);
+    this.setState({
+      data: this.refs.canvas.getSaveData(),
+    }, () => alert(this.state.data));
+    // // let savedData = this.refs.canvas.getSaveData();
+    // alert(this.state.data);
   }
   render() {
     return (
       <div className="App" onClick={() => this.handleClick}>
-        {/* <SketchFieldDemo /> */}
         <CanvasDraw ref="canvas" />
         <button onClick={() => this.handleClick()}>Save</button>
       </div>
