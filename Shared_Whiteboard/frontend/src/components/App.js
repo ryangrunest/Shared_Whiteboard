65f17b73fc11a43ba35ad6d9c023431799fd9649
Ryan Grunest <ryangrunest45@gmail.com>
2019-08-12 18:41:43 -0700

    successfully retrieve data from db, update components and state based on retrieved data

--- Shared_Whiteboard/frontend/src/components/App.js
+++ Shared_Whiteboard/frontend/src/components/App.js
@@ -1,44 +1,75 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import CanvasDraw from 'react-canvas-draw';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+import CanvasDraw from "react-canvas-draw";
+import axios from "axios";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       data: ""
-    }
+    };
   }
 
-  // to load data 
-  loadData() {
-    let dbData; // returned data from db
-    this.setState({
-      data: dbData
-    }, () => this.refs.canvas.loadSaveData(this.state.dbData, true))
-    // loadSaveData(saveData:String, immediata:Boolean) boolean flag loads immediately instead of live drawing it
+  componentDidMount() {
+    this.loadData();
   }
-  
+
+  // to load data
+  loadData() {
+    axios.get("http://localhost:8000/api/lines/").then(res => {
+      let dbData = res.data[res.data.length - 1].line; // returned data from DB
+      if (this.state.data != dbData) {
+        this.setState(
+          {
+            data: dbData
+          },
+          () => {
+            // loadSaveData(saveData:String, immediata:Boolean) boolean flag loads immediately instead of live drawing it
+            this.refs.canvas.loadSaveData(this.state.data, true);
+          }
+        );
+      }
+
+      console.log("get request complete!");
+    });
+    console.log("loadData finished!");
+  }
+
   handleClick(e) {
     e.preventDefault();
     // store the draw data in state
-    this.setState({
-      // this.refs.canvas.getSaveData is a function within the CanvasDraw child component
-      data: this.refs.canvas.getSaveData(),
-      // then alert the state
-    }, () => alert(this.state.data));
+    let savedData = this.refs.canvas.getSaveData();
+    console.log(savedData);
+    axios.post("api/lines/", { line: savedData }).then(() => {
+      this.setState(
+        {
+          // this.refs.canvas.getSaveData is a function within the CanvasDraw child component
+          data: savedData
+        },
+        () => {
+          this.loadData();
+        }
+      );
+    });
   }
 
   render() {
     return (
       <div className="App" onClick={() => this.handleClick}>
         <CanvasDraw
-          ref="canvas" 
+          ref="canvas"
           canvasWidth={document.documentElement.clientWidth * 0.8}
           canvasHeight={document.documentElement.clientHeight * 0.8}
           lazyRadius={2}
         />
-        <button className="App-submit-btn" onClick={(event) => this.handleClick(event)}>Save</button>
+        <button
+          className="App-submit-btn"
+          onClick={event => this.handleClick(event)}
+        >
+          Save
+        </button>
       </div>
     );
   }
